a6977cee3c50968ffcfb5568f6a89adb67718dee
Bruno Ferreira <bruno.ferreira_13@outlook.com>
2019-12-07 16:41:47 -0300

    feat: Added compression to API :sparkles: :heavy_plus_sign:

--- api/app.js
+++ api/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const helmet = require("helmet");
+const compression = require("compression");
 const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(helmet());
+app.use(compression());
 
 app.use(express.static("build"));
 app.use("/data", express.static("data"));
