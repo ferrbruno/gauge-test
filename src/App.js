85bf4b29f35f9c00e7749f05b3ac758672661abc
Bruno Ferreira <bruno.ferreira_13@outlook.com>
2019-12-07 13:54:47 -0300

    refactor: Turned chart into a component :recycle:

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import NavBar from "./components/NavBar";
-import Highcharts from "highcharts";
-import HighchartsReact from "highcharts-react-official";
+import Chart from "./components/Chart";
 import { CssBaseline } from "@material-ui/core";
 
 const getUsers = async () => {
@@ -25,69 +24,6 @@ const getData = async () => {
   return { users, interactions };
 };
 
-const buildChart = (users, interactions) => {
-  if (!users || !interactions) return;
-
-  const result = users.map(({ id, login: { username: userName } }) => {
-    const userInteractions = interactions.filter(({ user }) => {
-      return id === user;
-    });
-
-    return { userName, userInteractions };
-  });
-
-  result.sort((a, b) => b.userInteractions.length - a.userInteractions.length);
-
-  console.log(result);
-
-  const options = {
-    chart: {
-      type: "column"
-    },
-    xAxis: {
-      type: "category",
-      labels: {
-        rotation: -45,
-        style: {
-          fontSize: "13px",
-          fontFamily: "Verdana, sans-serif"
-        }
-      }
-    },
-    yAxis: {
-      allowDecimals: false,
-      title: {
-        text: "Interactions"
-      }
-    },
-    title: {
-      text: "User Interactions"
-    },
-    legend: {
-      enabled: false
-    },
-    series: [{
-      name: "Interactions",
-      data: result.map(({ userName, userInteractions: { length } }) => {
-        return [userName, length];
-      })
-    }],
-    dataLabels: {
-      enabled: true,
-      rotation: -90,
-      color: "#FFFFFF",
-      align: "right",
-      format: "{point.y} Interactions",
-      y: 10, // 10 pixels down from the top
-      style: {
-        fontSize: "13px"
-      }
-    }
-  };
-
-  return <HighchartsReact highcharts={Highcharts} options={options} />;
-};
-
 export default function App() {
   const [users, setUsers] = useState();
   const [interactions, setInteractions] = useState();
@@ -103,7 +39,7 @@ export default function App() {
     <>
       <CssBaseline />
       <NavBar />
-      {buildChart(users, interactions)}
+      <Chart users={users} interactions={interactions} />
     </>
   );
 }
