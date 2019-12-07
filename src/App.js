198cbce60be3a46a207c1c930ab0841f8cd11d5d
Bruno Ferreira <bruno.ferreira@climatempo.com.br>
2019-12-07 01:36:06 -0300

    Added simple chart to main page :sparkles: :construction:

--- src/App.js
+++ src/App.js
@@ -1,26 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import NavBar from "./components/NavBar";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+import { CssBaseline } from "@material-ui/core";
 
-function App() {
+const getUsers = async () => {
+  const response = await fetch("/data/users.json");
+  const json = await response.json();
+
+  return json;
+};
+
+const getInteractions = async () => {
+  const response = await fetch("/data/interactions.json");
+  const json = await response.json();
+
+  return json;
+};
+
+const getData = async () => {
+  const users = await getUsers();
+  const interactions = await getInteractions();
+
+  return { users, interactions };
+};
+
+const buildChart = (users, interactions) => {
+  if (!users || !interactions) return;
+
+  const result = users.map(({ id, login: { username: userName } }) => {
+    const userInteractions = interactions.filter(({ user }) => {
+      return id === user;
+    });
+
+    return { userName, userInteractions };
+  });
+
+  result.sort((a, b) => b.userInteractions.length - a.userInteractions.length);
+
+  console.log(result);
+
+  const options = {
+    chart: {
+      type: "column"
+    },
+    yAxis: {
+      title: {
+        text: "Interactions"
+      }
+    },
+    xAxis: {
+      title: {
+        text: "Users"
+      }
+    },
+    title: {
+      text: "User Interactions"
+    },
+    series: result.map(({ userName, userInteractions: { length } }) => {
+      return {
+        name: userName,
+        data: [length]
+      };
+    }),
+    dataLabels: {
+      enabled: true,
+      rotation: -90,
+      color: "#FFFFFF",
+      align: "right",
+      format: "{point.y} Interactions",
+      y: 10, // 10 pixels down from the top
+      style: {
+        fontSize: "13px"
+      }
+    }
+  };
+
+  return <HighchartsReact highcharts={Highcharts} options={options} />;
+};
+
+export default function App() {
+  const [users, setUsers] = useState();
+  const [interactions, setInteractions] = useState();
+
+  useEffect(() => {
+    getData().then(({ users, interactions }) => {
+      setUsers(users);
+      setInteractions(interactions);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <NavBar />
+      {buildChart(users, interactions)}
+    </>
   );
 }
-
-export default App;
