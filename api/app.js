3862707c7d7a860e9f206754e98c8b08d1a8f2ab
Bruno Ferreira <bruno.ferreira_13@outlook.com>
2019-12-07 15:52:18 -0300

    feat: Added helmet to API :sparkles:

--- api/app.js
+++ api/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const helmet = require("helmet");
 const app = express();
 const port = process.env.PORT || 8000;
 
+app.use(helmet());
+
+app.use(express.static("build"));
 app.use("/data", express.static("data"));
 
 app.listen(port, () => console.log(`Server listening on port ${port}.`));
